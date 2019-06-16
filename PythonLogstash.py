ac60e5430b40ff9dab8ce1cffa5f2c5fc028b283
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2019-06-16 16:57:52 -0500

    Python Logging Application

--- PythonLogstash.py
+++ PythonLogstash.py
@@ -7,9 +7,19 @@ test_logger = logging.getLogger('python-logstash-logger')
 test_logger.setLevel(logging.INFO)
 test_logger.addHandler(logstash.LogstashHandler('35.171.25.13', 5959, version=1))
 
-test_logger.error('python-logstash: test logstash error message.')
-test_logger.info('python-logstash: test logstash info message.')
-test_logger.warning('python-logstash: test logstash warning message.')
+def addNumbers(x, y):
+    sum = x + y
+    return sum
+
+result1 = addNumbers(1, 2)
+result2 = addNumbers(2, 3)
+result3 = addNumbers(3, 4)
+result4 = addNumbers(4, 5)
+
+test_logger.info('Addition result 1: '.format(result1))
+test_logger.info('Addition result 2: '.format(result2))
+test_logger.info('Addition result 3: '.format(result3))
+test_logger.info('Addition result 4: '.format(result4))
 
 # add extra field to logstash message
 extra = {
